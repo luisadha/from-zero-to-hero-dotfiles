d704cf5178e0c6e07526f64cd278b8c22d38a9bb
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-07-10 01:19:33 +0700

    add .function.sh

---
+++ .function.sh
@@ -0,0 +1,5 @@
+# for bash preexec login shells sessions
+
+preexec() { echo -e "just typed $1\n"; }
+precmd_1() { echo -e "\n🔀 Suffle the songs, (\`unset precmd' or \`precmd_functions' to disable this feature)"; }
+precmd_2() { brandomusicv && drawercli; }
